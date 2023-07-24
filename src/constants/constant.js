6b457a6eb1f8a0914d27cc64c105da671aaad363
BALWANT SINGH <bintudhanda@outlook.com>
2023-07-24 14:22:43 +0530

    icon implement

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://192.168.1.5:5291/api/';
-export const News_URL = 'http://192.168.1.5:5291';
+// export const BASE_URL = 'http://192.168.1.5:5291/api/';
+// export const News_URL = 'http://192.168.1.5:5291';
 
-// export const BASE_URL = 'https://impacthisar.xyz/api/';
-// export const News_URL = 'https://impacthisar.xyz';
+export const BASE_URL = 'https://impacthisar.xyz/api/';
+export const News_URL = 'https://impacthisar.xyz';
