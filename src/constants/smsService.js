bab69b015d00c21f3bf7f5c8a907e7346eac7b85
dharmveerofficial <dharmvirofficial@gmail.com>
2024-04-17 22:50:40 +0530

    otp issue

--- src/constants/smsService.js
+++ src/constants/smsService.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
 import {Post as httpPost} from './httpService';
-export const sendOTP = (otp, mobile) => {
-  let msg = `Dear Student, Your Registration OTP is ${otp} Mobile No. 9050546000 Impact Academy, Hisar`;
-  // let user = 'impactcampus';
-  // let password = 'K9F2HDNY';
-  // let sid = 'IIMPCT';
-  // let apiURL = `http://www.getwaysms.com/vendorsms/pushsms.aspx?user=${user}&password=${password}&msisdn=${mobile}&sid=${sid}&msg=${msg}&fl=0&gwid=2`;
-  // return axios.get(apiURL);
+export const sendOTP = async (otp, mobile) => {
   const smsType = 'OTPSMS';
-  return httpPost('User/SendOTP', {number: mobile, otp, type: smsType});
+  const response = await httpPost('User/SendOTP', {number: mobile, msg: otp?.toString(), type: smsType})
+  console.log(response)
+  return response;
 };
 
 //http://www.getwaysms.com/vendorsms/pushsms.aspx?user=impactcampus&password=K9F2HDNY&msisdn=9416669174&sid=IMPHSR&msg=Dear Student, Your Registration OTP is 1234 Mobile No. 9050546000 Impact Academy, Hisar&fl=0&gwid=2
